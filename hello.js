9ec6a43f1d02f2b337dd6b1867e73cbd54ca4bf8
GuoCuiCow <867712764@qq.com>
2016-02-03 17:27:58 +0800

    2.2

--- hello.js
+++ hello.js
@@ -1,6 +1,10 @@
 var a = "a"
 <<<<<<< HEAD
+<<<<<<< HEAD
 var b = "a"
+=======
+var b = "b"
+>>>>>>> parent of facb698... Revert "1.1"
 =======
 var b = "b"
 >>>>>>> parent of facb698... Revert "1.1"
