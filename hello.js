182f02073aef5db488929963605cfdb7794b0bc7
GuoCuiCow <867712764@qq.com>
2016-02-03 17:47:06 +0800

    no message

--- hello.js
+++ hello.js
@@ -1,6 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 var a = "aaaa"
 =======
+=======
+>>>>>>> origin/master
 var a = "a"
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -11,4 +14,7 @@ var b = "b"
 =======
 var b = "b"
 >>>>>>> parent of facb698... Revert "1.1"
+<<<<<<< HEAD
 >>>>>>> 9ec6a43f1d02f2b337dd6b1867e73cbd54ca4bf8
+=======
+>>>>>>> origin/master
